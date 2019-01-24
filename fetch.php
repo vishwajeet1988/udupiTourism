36a8d86804768860648133f504c55f33abe92a1b
vishwajeet1988 <43531191+vishwajeet1988@users.noreply.github.com>
2019-01-24 10:20:40 +0530

    sql and php files

---
+++ fetch.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
+ 
+$con = @mysqli_connect('localhost', 'root', '', 'angular_db');
+
+if (!$con) {
+    echo "Error: " . mysqli_connect_error();
+	exit();
+}
+
+	$sql = "select * from cards";
+    $result = mysqli_query ($con,$sql);
+	while ($row = mysqli_fetch_assoc($result)) {
+		$output[]=$row;
+	}
+	print(json_encode($output));
+
+?>
